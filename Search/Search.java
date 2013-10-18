47ad9e49e9b4364cce473ace6024ff03f02eec25
Hao Zhou <hzhou@wpi.edu>
2013-10-17 21:59:56 -0400

    implement binarySearch

--- Search/Search.java
+++ Search/Search.java
@@ -2,14 +2,25 @@ public class Search{
 
 	public static int binarySearch(int[] array, int numberToBeFound){
 		int left = 0;
-		int right = array.lenght-1;
+		int right = array.length-1;
+		int middle;
 		while(left <= right){
-			
+			middle = (left+right)/2;
+			if(array[middle] == numberToBeFound){
+				return middle;
+			}
+			else if(array[middle] > numberToBeFound){
+				right = middle - 1;
+			} else {
+				left = middle + 1;
+			}
+
 		}
+		return -1;
 	}
 	public static void main(String argv[]){
-		int[] array = new int[]{0, 1, 2, 13, 23, 71, 120 433, 899};
-		int returnValue = binarySearch(array, 71);
+		int[] array = new int[]{0, 1, 2, 13, 23, 71, 120, 433, 899};
+		int returnValue = binarySearch(array, 721);
 		if( returnValue == -1){
 			System.out.println("No such number found in the array!");
 		} else {
